3e320fc54c5ba80ff392ccd2fea807b511cb1b99
moki-s <mokiviral@gmail.com>
2026-03-06 11:30:27 +0000

    chore(admindashboard): bump version to 1.2.2 for case study fix
    Version 2026030601, release 1.2.2.
    
    Co-Authored-By: moki-s <mokiviral@gmail.com>

--- local/admindashboard/version.php
+++ local/admindashboard/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_admindashboard';
-$plugin->version   = 2026020500;
+$plugin->version   = 2026030601;
 $plugin->requires  = 2024042200; // Moodle 4.4+.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.2.1';
+$plugin->release   = '1.2.2';
