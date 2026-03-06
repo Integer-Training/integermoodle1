2a96d785b965db782ef70a57af9043e091460899
moki-s <mokiviral@gmail.com>
2026-03-06 11:30:20 +0000

    chore(learner): bump version to 1.4.1 for case study marking fix
    Version 2026030601, release 1.4.1.
    
    Co-Authored-By: moki-s <mokiviral@gmail.com>

--- local/learner/version.php
+++ local/learner/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_learner';
-$plugin->version   = 2026030400;
+$plugin->version   = 2026030601;
 $plugin->requires  = 2024051700; // Moodle 5.0 minimum.
-$plugin->release   = '1.4.0';
+$plugin->release   = '1.4.1';
 $plugin->maturity  = MATURITY_STABLE;
